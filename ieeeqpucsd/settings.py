a857909e4d8f5ef7da88c4ddaaef37b56a162e70
hwanggit <luw055@ucsd.edu>
2019-09-09 11:35:17 -0700

    added database_url

--- ieeeqpucsd/settings.py
+++ ieeeqpucsd/settings.py
@@ -139,12 +139,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/landing/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'landing/static'),
-)
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
