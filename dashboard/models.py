20ec3548e0312009d5885f4de34ba078f034bd9b
hwanggit <luw055@ucsd.edu>
2019-09-25 14:37:09 -0700

    Fixed score field

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from landing.models import QPApplication
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class ScoreApplication(models.Model):
     application = models.ForeignKey(QPApplication, on_delete=models.DO_NOTHING)
     scorer = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    score = models.IntegerField()
+    score = models.IntegerField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
