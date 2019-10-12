c6f174b84687dc725aa9219be9faff3c7cc67cec
hwanggit <luw055@ucsd.edu>
2019-10-11 17:54:33 -0700

    redirect

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('scoring/<int:appid>', views.score_applicant, name='score_applicant'),
     path('rubric/', views.rubric, name='rubric'),
     path('teams/', views.teams, name='teams'),
-    path('mentors/', views.mentors, name='teams'),
+    path('mentors/', views.mentors, name='mentors'),
     path('documentation/', views.ProjectProposalCreate.as_view(), name='documentation'),
     path('documentation/success/', views.documentation_success, name='documentation_success'),
 ]
